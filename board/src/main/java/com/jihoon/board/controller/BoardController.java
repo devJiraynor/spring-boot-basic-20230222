abe2789d452c60c5c4c3eba40a0f85fad3a0207f
devJiraynor <jiraynor@naver.com>
2023-04-27 11:04:06 +0900

    2023.04.27. 10:50

--- board/src/main/java/com/jihoon/board/controller/BoardController.java
+++ board/src/main/java/com/jihoon/board/controller/BoardController.java
@@ -1,8 +1,36 @@
 package com.jihoon.board.controller;
 
+import javax.validation.Valid;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.jihoon.board.dto.request.board.PostBoardRequestDto;
+import com.jihoon.board.dto.response.ResponseDto;
+import com.jihoon.board.service.BoardService;
+
 @RestController
+@RequestMapping("/api/v1/board")
 public class BoardController {
     
+    private BoardService boardService;
+
+    @Autowired
+    public BoardController(BoardService boardService) {
+        this.boardService = boardService;
+    }
+
+    //* 1.게시물 작성 */
+    @PostMapping("")
+    public ResponseEntity<ResponseDto> postBoard(
+        @Valid @RequestBody PostBoardRequestDto requestBody
+    ) {
+        ResponseEntity<ResponseDto> response = boardService.postBoard(requestBody);
+        return response;
+    }
+
 }
--- board/src/main/java/com/jihoon/board/service/BoardService.java
+++ board/src/main/java/com/jihoon/board/service/BoardService.java
@@ -1,5 +1,18 @@
 package com.jihoon.board.service;
 
+import org.springframework.http.ResponseEntity;
+
+import com.jihoon.board.dto.request.board.PatchBoardRequestDto;
+import com.jihoon.board.dto.request.board.PostBoardRequestDto;
+import com.jihoon.board.dto.response.board.GetBoardResponseDto;
+import com.jihoon.board.dto.response.board.GetBoardListResponseDto;
+import com.jihoon.board.dto.response.ResponseDto;
+
 public interface BoardService {
-    
+    public ResponseEntity<ResponseDto> postBoard(PostBoardRequestDto dto);
+    public ResponseEntity<? super GetBoardResponseDto> getBoard(Integer boardNumber);
+    public ResponseEntity<? super GetBoardListResponseDto> getBoardList();
+    public ResponseEntity<? super GetBoardListResponseDto> getBoardTop3();
+    public ResponseEntity<ResponseDto> patchBoard(PatchBoardRequestDto dto);
+    public ResponseEntity<ResponseDto> deleteBoard(String userEmail, Integer boardNumber);
 }

06ed6cb7b4b9eeb6924a6107d67326ce05ee20a2
devJiraynor <jiraynor@naver.com>
2023-05-09 10:52:56 +0900

    2023.05.09. 10:50

---
+++ firstproject/src/main/java/com/jihoon/firstproject/common/constant/RequestPattern.java
@@ -0,0 +1,5 @@
+package com.jihoon.firstproject.common.constant;
+
+public interface RequestPattern {
+    public static final String FILE_API = "file";
+}
--- firstproject/src/main/java/com/jihoon/firstproject/controller/FileController.java
+++ firstproject/src/main/java/com/jihoon/firstproject/controller/FileController.java
@@ -1,21 +1,45 @@
 package com.jihoon.firstproject.controller;
 
+import org.springframework.core.io.Resource;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
+import com.jihoon.firstproject.common.constant.RequestPattern;
+import com.jihoon.firstproject.service.FileService;
+
+import lombok.RequiredArgsConstructor;
+
 @RestController
-@RequestMapping("file")
+@RequestMapping(RequestPattern.FILE_API)
+@RequiredArgsConstructor
 public class FileController {
+
+    private final FileService fileService;
+
+    private final String UPLOAD_URL = "upload";
+    private final String GET_URL = "{fileName}";
     
     //* 파일 업로드 */
-    @PostMapping("upload")
+    @PostMapping(UPLOAD_URL)
     public String upload(
         @RequestParam("file") MultipartFile file
     ) {
-        return file.getOriginalFilename();
+        return fileService.upload(file);
+    }
+
+    //* 파일 불러오기 */
+    @GetMapping(value=GET_URL, produces={MediaType.IMAGE_PNG_VALUE, MediaType.IMAGE_JPEG_VALUE})
+    public Resource getFile(
+        @PathVariable("fileName") String fileName
+    ) {
+        
+        return fileService.getFile(fileName);
     }
 
 }

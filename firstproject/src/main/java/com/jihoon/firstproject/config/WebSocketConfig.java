d4fdeeca345edb49ac08b87c812e08d51547ff92
devJiraynor <jiraynor@naver.com>
2023-05-24 11:37:57 +0900

    2023.05.24. 11:30

--- firstproject/src/main/java/com/jihoon/firstproject/config/WebSocketConfig.java
+++ firstproject/src/main/java/com/jihoon/firstproject/config/WebSocketConfig.java
@@ -5,13 +5,24 @@ import org.springframework.web.socket.config.annotation.EnableWebSocket;
 import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
 import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
 
-@EnableWebSocket
+import com.jihoon.firstproject.provider.WebSocketProvider;
+
+import lombok.RequiredArgsConstructor;
+
 @Configuration
+@EnableWebSocket
+@RequiredArgsConstructor
 public class WebSocketConfig implements WebSocketConfigurer {
+
+    private final WebSocketProvider webSocketProvider;
 
     @Override
     public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
         
+        registry
+            .addHandler(webSocketProvider, "/web-socket")
+            .setAllowedOrigins("*");
+
     }
     
 }
